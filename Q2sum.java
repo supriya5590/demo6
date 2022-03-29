b6c42fdd39b9a6d5346af8b234a25e4d613000a9
Supriya11 <101583227+supriya5590@users.noreply.github.com>
2022-03-29 16:15:49 +0530

    Add files via upload

---
+++ Q2sum.java
@@ -0,0 +1,15 @@
+
+//Q 2 write a program that defines two variables and print their sum .
+
+package practiceASMT1;
+
+public class Q2sum {
+
+	public static void main(String[] args) {
+		int a=56;
+		int b=345;
+		int sum=a+b;
+		System.out.println("The sum is="+sum);
+	}
+
+}
